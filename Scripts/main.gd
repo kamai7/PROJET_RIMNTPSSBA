afb0d260b32f34afaf59b798b3a680b971d80169
helori_ollivier <helori.ollivier@free.fr>
2024-12-06 03:02:48 +0100

    check organe order

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,19 +1,51 @@
 extends CharacterBody2D
 
-const SPEED = 300
+const SPEED = 5000
+
+@onready var organe := get_tree().get_current_scene().get_node("Organe") as TileMapLayer
+
+var checkpoint = 0
+var checkpoint_names = ["poumons", "coeur", "cerveau","end"]
 
 func _physics_process(delta: float) -> void:
-	check_path()
-	
-	var dir = to_local($NavigationAgent2D.get_next_path_position())
-	velocity = dir * SPEED * delta
-	
-	move_and_slide()
+	if check_path(delta):
+		print("win")
+	else:
+		print("try again")
 
-func check_path():
+func check_path(delta) -> bool:
+	
+	var is_connected = check_connection()
+	
+	var dir = to_local($NavigationAgent2D.get_next_path_position()).normalized()
+	velocity = dir * SPEED * delta
+	move_and_slide()
+	
+	var name = get_tile(organe)
+	#print("name: ", name, "checkpoint: ", checkpoint_names[checkpoint])
+	
+	if name == checkpoint_names[checkpoint]:
+		checkpoint += 1
+	
+	if is_connected and checkpoint == 3:
+		return true
+	else:
+		return false
+		
+func check_connection() -> bool:
 	
 	$NavigationAgent2D.target_position = Vector2i(3*32,0)
 	var res = $NavigationAgent2D.is_target_reachable()
-	print(res)
 	
+	return res
+
+func get_tile(tilemap: TileMapLayer) -> String:
 	
+	var pos = Vector2i(int(position.x / 32), int(position.y / 32))
+	var tile = tilemap.get_cell_tile_data(pos)
+	
+	var data = ""
+	if tile != null:
+		data = tile.get_custom_data("identifiant")
+	
+	return data
